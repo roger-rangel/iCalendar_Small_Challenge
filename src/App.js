6732012b1d97abea2c155794b42e611b6fd9ecb1
Roger Rangel <roger.rangel.ee@gmail.com>
2022-05-06 14:34:25 +0200

    Add New Event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Login from './Views/LogIn/login';
 
 
+import NewEvent from './Views/Pages/NewEvent/newEvent';
+import ViewEvents from './Views/Pages/ViewEvents/viewEvents';
+
+
+
 function App() {
-  return (
-    <>
-    <Login />
-    </>
-  );
-}
+  const { auth } = useSelector(state => state);
+  const [nav, setNav] = useState(false)
+  const value = {nav, setNav}
+  
+    return (
+      <>
+        {!auth.LoggedIn ? <Login /> :
+          <div className='App'>
+            <h1>App</h1>
+          </div>
+        }
+      </>
+    );
+  }
+
 
 export default App;
